2ebd16a95516801fbb0c3848240073aaa8fbe518
Yago <ylm@icomp.ufam.edu.br>
2024-09-05 18:05:24 -0400

    chore: Create route '/' and add nodemon

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
