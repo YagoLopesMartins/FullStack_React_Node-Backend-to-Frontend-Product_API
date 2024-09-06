4f62a04f34776b8f2479b685bd6bce57464d983c
Yago <ylm@icomp.ufam.edu.br>
2024-09-06 01:18:42 -0400

    chore: Setup project and add react-router-dom, icons and simple components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
+}
+
+export default App;
