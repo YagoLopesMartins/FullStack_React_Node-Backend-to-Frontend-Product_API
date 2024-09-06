5de695ca43937207caf5db17e15b17487a25be93
Yago <ylm@icomp.ufam.edu.br>
2024-09-06 14:51:02 -0400

    feat: Add delete icon and logical to Delete APi

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -24,4 +24,17 @@ export const useProductStore = create((set) => ({
     const data = await res.json();
     set({ products: data.data });
   },
+  deleteProduct: async (pid) => {
+    const res = await fetch(`/api/products/${pid}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (!data.success) return { success: false, message: data.message };
+
+    // update the ui immediately, without needing a refresh
+    set((state) => ({
+      products: state.products.filter((product) => product._id !== pid),
+    }));
+    return { success: true, message: data.message };
+  },
 }));
