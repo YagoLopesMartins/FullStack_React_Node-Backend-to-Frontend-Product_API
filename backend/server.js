b28bcb52fa7c9ecfff9db8e9ce76177e9e01b0f4
Yago <ylm@icomp.ufam.edu.br>
2024-09-06 17:41:46 -0400

    chore: Config project  to deployment

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,25 @@ import { connectDB } from "./config/db.js";
 
 import productRoutes from "./routes/product.route.js";
 
+import path from "path";
+
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
